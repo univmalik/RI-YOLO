1f7ba9f4ba1041b70aa477deff7a76d440f0a854
ADEEL MALIK <43076996+univmalik@users.noreply.github.com>
2019-07-26 15:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,47 +2,50 @@ import TextRotation
 import fileHandle
 import ImageRotation
 
-listFileTXT = fileHandle.files_LTXT("C:/Users/Administrator/PycharmProjects/openCV/img/")
-listFileJPG = fileHandle.files_LJPG("C:/Users/Administrator/PycharmProjects/openCV/img/")
+# Enter file address here like this
+# textFileAddress = "C:/Users/Administrator/PycharmProjects/openCV/img/"
+textFileAddress = ""
+jpgFileAddress = ""
+
+listFileTXT = fileHandle.files_LTXT(textFileAddress)
+listFileJPG = fileHandle.files_LJPG(jpgFileAddress)
 
 for file in listFileJPG:
-      ImageRotation.rotImage(file)
+    ImageRotation.rotImage(file)
 
-#update list
-listFileJPG = fileHandle.files_LJPG("C:/Users/Administrator/PycharmProjects/openCV/img/")
+# update list
+listFileJPG = fileHandle.files_LJPG(jpgFileAddress)
 
 for file in listFileJPG:
     ImageRotation.rotImage180(file)
 
-listFileJPG = fileHandle.files_LJPG("C:/Users/Administrator/PycharmProjects/openCV/img/")
+listFileJPG = fileHandle.files_LJPG(jpgFileAddress)
 
 for file in listFileJPG:
     ImageRotation.rotImage270(file)
 
-listFileJPG = fileHandle.files_LJPG("C:/Users/Administrator/PycharmProjects/openCV/img/")
 
 for file in listFileTXT:
-     TextRotation.rotTextC(file)
+    TextRotation.rotTextC(file)
 
-listFileTXT = fileHandle.files_LTXT("C:/Users/Administrator/PycharmProjects/openCV/img/")
-
-for file in listFileTXT:
-     TextRotation.rotTextC180(file)
-
-listFileTXT = fileHandle.files_LTXT("C:/Users/Administrator/PycharmProjects/openCV/img/")
+listFileTXT = fileHandle.files_LTXT(textFileAddress)
 
 for file in listFileTXT:
-     TextRotation.rotTextC270(file)
+    TextRotation.rotTextC180(file)
+
+listFileTXT = fileHandle.files_LTXT(textFileAddress)
+
+for file in listFileTXT:
+    TextRotation.rotTextC270(file)
 
 TextRotation.rotTextC("cv.txt")
 TextRotation.rotTextC180("cv-ROT90.txt")
 TextRotation.rotTextC270("cv-ROT180.txt")
 
-
 #
-#TextRotation.rotTextC("cv.txt")
+# TextRotation.rotTextC("cv.txt")
 
-#TextRotation.rotTextC("cv.txt")
+# TextRotation.rotTextC("cv.txt")
 
 
 # test = "0 0.255674 0.565456 0.752567 0.667356\n" + \
@@ -62,13 +65,3 @@ TextRotation.rotTextC270("cv-ROT180.txt")
 # intCList = TextRotation.IntCLIST(rotList)
 #
 # finalStr = TextRotation.finalString(intCList)
-
-
-
-
-
-
-
-
-
-
